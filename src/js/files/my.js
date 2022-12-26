a37bf644ea52b08a51fa2662a49d6a5c24b27e3b
Дмитрий <osdal67@gmail.com>
2022-12-26 11:05:32 +0200

    Первое приближение. Сверстал страницу и сайдбар

---
+++ src/js/files/my.js
@@ -0,0 +1,14 @@
+// Убираем отступ в первой строке содержимого страницы
+let paragraphInPage = document.querySelector('.page__content p');
+paragraphInPage.style.textIndent = 0;
+
+// Ограничиваем высоту сайдбара
+// let sideBar = document.querySelector('.page__sidebar');
+// let heigtSidebar = 0;
+
+// for (let i = 0; sideBar.length; i++) {
+// 	heigtSidebar = heigtSidebar + sideBar[i].style.height;
+// 	console.log(heigtSidebar);
+// }
+// sideBar.style.height = heigtSidebar;
+
